b6af771025b613f462810137e1173b2ebfb25737
Anderson Moralez <anderson.moralez@hotmail.com>
2020-06-04 14:21:15 -0300

    started project

---
+++ SOAP/src/com/Pessoa.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Pessoa {
+	private Integer idade = 10;
+	public String fun(String nome) { return "nome: " + String.valueOf(nome) + 
+									" idade: " + this.idade;}
+}
